f0890f9d25aa3f7f054a5d4eb153f57709adb165
Alexandr Krasnij <php4.drvn@gmail.com>
2019-03-29 09:20:58 +0200

    Added support partials

--- src/plugin.js
+++ src/plugin.js
@@ -3,6 +3,9 @@ export default (ctx, inject) => {
   <%for (var key in options.vuefrontConfig.components) {%>
     Vue.component('vf<%= key %>', require('<%= options.vuefrontConfig.components[key] %>').default)
   <%}%>
+    <%for (var key in options.vuefrontConfig.partials) {%>
+    Vue.component('vfPartial<%= key %>', require('<%= options.vuefrontConfig.partials[key] %>').default)
+  <%}%>
 
   <%for (var key in options.vuefrontConfig.modules) {%>
     Vue.component('vfModule<%= key %>', require('<%= options.vuefrontConfig.modules[key] %>').default)
