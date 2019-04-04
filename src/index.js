55163dcb049f069c4a88ae3e3125f4585949d4c9
Alexandr Krasnij <php4.drvn@gmail.com>
2019-04-04 13:26:18 +0300

    Added support theme

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
-const path = require('path');
+const path = require('path')
+let themeOptions = require('vuefront-core')
+import _ from 'lodash'
+
+const mergeConfig = (objValue, srcValue) => {
+    if (_.isArray(objValue)) {
+        return objValue.concat(srcValue)
+    } else if (_.isObject(objValue)) {
+        return _.merge(objValue, srcValue)
+    } else {
+        return srcValue
+    }
+}
 
 export default function vuefrontModule() {
     const config = require(this.options.rootDir + '/vuefront.config').default
+    if (typeof config.theme !== 'undefined') {
+        const customThemeOptions = require(config.theme).default
+        themeOptions = _.mergeWith(themeOptions, customThemeOptions, mergeConfig)
+    }
+    themeOptions = _.mergeWith(themeOptions, config, mergeConfig)
     this.addPlugin({
         src: path.resolve(__dirname, './plugin.js'),
         options: {
-            vuefrontConfig: config,
+            vuefrontConfig: themeOptions,
             debug: this.options.dev
         }
     })
