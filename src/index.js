7b94a81d0df6826b38c1f5b66c219ac4cb7c20a9
Alexandr Krasnij <php4.drvn@gmail.com>
2019-04-04 10:46:53 +0300

    Added plugins support

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const path = require('path');
+
 export default function vuefrontModule() {
-  const config = require(this.options.rootDir + '/vuefront.config').default;
-  this.addPlugin({
-    src: path.resolve(__dirname, './plugin.js'),
-    options: {
-      vuefrontConfig: config,
-      debug: this.options.dev
+    const config = require(this.options.rootDir + '/vuefront.config').default
+    if (typeof config['plugins'] !== 'undefined') {
+        for (const key in config['plugins']) {
+            this.addPlugin({
+                src: config.plugins[key]
+            })
+        }
     }
-  })
+    this.addPlugin({
+        src: path.resolve(__dirname, './plugin.js'),
+        options: {
+            vuefrontConfig: config,
+            debug: this.options.dev
+        }
+    })
 }
