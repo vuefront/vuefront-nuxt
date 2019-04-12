c2b2a97a353c41cece4544afef5de83a67e6c3dc
Alexandr Krasnij <php4.drvn@gmail.com>
2019-04-12 13:43:52 +0300

    Added support store

--- src/plugin.js
+++ src/plugin.js
@@ -1,16 +1,33 @@
 import Vue from 'vue'
+
 <%for (var key in options.vuefrontConfig.plugins) {%>
   require('<%= options.vuefrontConfig.plugins[key] %>')
 <%}%>
 <%for (var key in options.vuefrontConfig.css) {%>
     require('<%= options.vuefrontConfig.css[key] %>')
 <%}%>
-export default (ctx, inject) => {
+
+export default async (ctx, inject) => {
   const components = {
       element: {},
       template: {},
       position: {},
       module: {}
+  }
+
+  <%for (var key in options.vuefrontConfig.store) {%>
+    <% if (typeof options.vuefrontConfig.store[key].module !== 'undefined') { %>
+        ctx.store.registerModule(<%= JSON.stringify(options.vuefrontConfig.store[key].path) %>, {namespaced: true, ...require('<%= options.vuefrontConfig.store[key].module %>')})
+    <% } else {%>
+        ctx.store.registerModule(<%= JSON.stringify(options.vuefrontConfig.store[key].path) %>, {namespaced: true})
+    <% } %>
+
+  <%}%>
+  
+  if(process.server) {
+    await ctx.store.dispatch('vuefront/nuxtServerInit', ctx)
+  } else {
+    await ctx.store.dispatch('vuefront/nuxtClientInit', ctx)
   }
   <%for (var key in options.vuefrontConfig.components) {%>
     components['vf<%= key %>'] = Vue.component('vf<%= key %>', require('<%= options.vuefrontConfig.components[key] %>').default)
